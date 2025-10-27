44e2607c2086df674bdf7b64f1d79109e7439f7b
datnt171874 <danielsmith2511@gmail.com>
2025-10-27 07:19:47 +0700

    feat: Update request body (roleName) for Register function

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -13,7 +13,8 @@ export const register = async (req, res) => {
         const { 
             email, 
             password, 
-            confirmPassword
+            confirmPassword,
+            roleName
         } = req.body;
 
         
@@ -57,7 +58,7 @@ export const register = async (req, res) => {
             fullName: "", 
             email,
             passwordHash,
-            roleName: "CUSTOMER", 
+            roleName, 
             phoneNumber: "",
             address: "",
             dateOfBirth: null
