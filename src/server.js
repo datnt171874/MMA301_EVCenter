30fc425d705212afc9c7e9f06624df6416652a27
datnt171874 <danielsmith2511@gmail.com>
2025-10-27 07:52:29 +0700

    feat: Update APIs on Swagger

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ const PORT =  process.env.PORT || 3000;
 
 app.use(express.json());
 
-// Swagger
+// Swagger: http://localhost:3000/api-docs/
 app.use("/api-docs", swaggerServe, swaggerSetup);
 
 
